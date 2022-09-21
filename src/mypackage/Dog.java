c873c12a57d39992b11769ad7ae8abc15e1efc77
Ankit7347 <ankitsri7347@gmail.com>
2022-09-21 14:13:55 +0530

    DOG class

--- src/mypackage/Dog.java
+++ src/mypackage/Dog.java
@@ -4,12 +4,25 @@ public class Dog {
     public String breed;
     public int age;
     public String color;
-
-    public void bark(){
-        System.out.println("DOG bark Whooh Whooh!");
+    public String bark;
+    public Dog(){
+        System.out.println("Default constructor");
     }
-    public String eat(){
-        return("Milk and chappati");
+
+    public Dog(String Breed,String Color,String Bark,int Age){
+        //doesn't have void and return type
+        System.out.println("Parameterized constructor");
+        breed=Breed;
+        color=Color;
+        bark=Bark;
+        age=Age;
+    }
+
+    public void eat(){
+        System.out.println("Milk and Curd");
+    }
+    public String bark(){
+        return(bark);
 
     }
 }
--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,15 +1,32 @@
 package mypackage;
 
 public class Main {
-    public static void main(String arg[]){
-        Dog dog1= new Dog();
-        dog1.age=4;
-        dog1.breed="Undefined";
-        dog1.color="WhiteBrown";
+    public static void main(String[] arg){
+        Dog dog1= new Dog();//Default Constructor
 
-        dog1.bark();
+        dog1.breed="Undefined";
+        dog1.age=4;
+        dog1.color="WhiteBrown";
+        dog1.bark="Whooh! Whooh";
+
+        System.out.println(dog1.breed);
+        System.out.println(dog1.age);
+        System.out.println(dog1.color);
         System.out.print("Dog eats: ");
-        System.out.println(dog1.eat());
+        dog1.eat();
+        System.out.println(dog1.bark());
+
+
+        Dog dog2 =new Dog("Shephard","Black_Brown","Wau",6);
+        System.out.println(dog2.breed);
+        System.out.println(dog2.age);
+        System.out.println(dog2.color);
+
+        System.out.print("Dog eats: ");
+        dog2.eat();
+        System.out.println(dog2.bark());
+
+
 
     }
 
