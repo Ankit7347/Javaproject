175a592c1882d2501ba9dc0e9e6f6b5475b151d1
Ankit7347 <101862004+Ankit7347@users.noreply.github.com>
2022-09-21 16:20:40 +0530

    Update This.java

--- src/thiskeyword/This.java
+++ src/thiskeyword/This.java
@@ -1,18 +1,18 @@
 package thiskeyword;
-
+//this class runs first then main run
 public class This {
     public float length;
     public float breadth;
 
-    public This(){
-        this(20,30);
+    public This(){              //default constructor
+        this(20,30); //call the paremeterised constructor as value is passed and  then the value stored in instance variable
         System.out.println("Inside This");
     }
-    public  This(float length){
+    public  This(float length){   //parameterised constructor
         this.length=length;
         System.out.println("Inside This1");
     }
-    public This(float length,float breadth){
+    public This(float length,float breadth){ //parameterised constructor
         this.length=length;
         this.breadth=breadth;
         System.out.println("Inside This2");
