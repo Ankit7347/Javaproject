81eb0367261fb1b1c3c572b15e2ac479ace2f3cb
Ankit7347 <ankitsri7347@gmail.com>
2022-09-21 13:31:23 +0530

    DOG class

---
+++ src/mypackage/Dog.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class Dog {
+    public String breed;
+    public int age;
+    public String color;
+
+    public void bark(){
+        System.out.println("DOG bark Whooh Whooh!");
+    }
+    public String eat(){
+        return("Milk and chappati");
+    }
+}
--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,4 +1,15 @@
 package mypackage;
 
 public class Main {
+    public static void main(String arg[]){
+        Dog dog1= new Dog();
+        dog1.age=4;
+        dog1.breed="Undefined";
+        dog1.color="WhiteBrown";
+
+        dog1.bark();
+        System.out.print("Dog eats: ");
+        System.out.println(dog1.eat());
+    }
+
 }
